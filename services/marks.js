2926aaa2d758ef0a14461ce2585d02c6f9e05819
Raman_Nekhaichyk <Raman_Nekhaichyk@epam.com>
2020-04-10 11:55:50 +0300

    feat: add batch adding for the marks

--- services/marks.js
+++ services/marks.js
@@ -8,21 +8,42 @@ exports.getAll = async function () {
 
 exports.createMark = async function (mark = {}) {
 
-    return (new Mark(mark)).save();
+    if (Array.isArray(mark)) {
+        const handler = mark.reduce((acc, mark) => {
+            mark._id ? acc.update.push(mark) : acc.create.push(mark);
+            return acc;
+        }, {create: [], update: []});
+
+        // create new
+        const created =  await Mark.insertMany(handler.create);
+
+        // update old
+        handler.update.reduce(
+            (acc, current) => {
+                const updated = exports.updateMark(current);
+                acc = [...acc, updated];
+                return acc;
+            }, []
+        );
+
+        return created;
+
+
+    } else {
+        return (new Mark(mark)).save();
+    }
+
+
 
 };
 
 exports.updateMark = async function (mark) {
-
-    console.log("UPDATE:", mark);
 
     return await Mark.updateOne({_id: mark.id}, {...mark})
 
 };
 
 exports.deleteMark = async function(id) {
-
-    console.log("DELETE:", id);
 
     return await exports.updateMark({id, _deletedAt: Date.now()});
 
